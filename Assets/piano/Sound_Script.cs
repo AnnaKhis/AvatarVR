5b09d4a0bbd6f316590cf77725de22968df04752
AnnaKhis <anna.khismatulina@skoltech.ru>
2020-05-22 18:09:58 +0300

    22.05(2)

---
+++ Assets/piano/Sound_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Script : MonoBehaviour
+{
+    public AudioSource NoteSound;
+    
+    private void OnCollisionEnter(Collision Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            NoteSound.Play();   
+        }
+    }
+}
