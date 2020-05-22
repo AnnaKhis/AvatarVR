1614e2b188814b71ec9efc5009f174019c94ae4c
AnnaKhis <anna.khismatulina@skoltech.ru>
2020-05-22 15:26:58 +0300

    22.05 (1)

---
+++ Assets/KeyCollider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollider : MonoBehaviour
+{
+    public Vector3 InitialState;
+    void OnCollisionEnter(Collision Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            if (InitialState == this.transform.position)
+            {
+                if (this.tag == "WhiteKey")
+                {
+                    this.transform.position -= new Vector3(0, 0.012f, 0); 
+                }
+                else{this.transform.position -= new Vector3(0, 0.007f, 0);}
+            }
+        }
+        
+    }
+
+    private void OnCollisionExit(Collision Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            if (InitialState != this.transform.position)
+            {
+                this.transform.position = InitialState;
+            }
+        }
+    }
+
+    public void Start()
+    {
+        InitialState = this.transform.position;
+    }
+}
+
